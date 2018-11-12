72835c8797b9c147a0cdf046f78d152442604573
M.Roman <moshchenkov.r@cronix.ms>
2018-11-12 11:00:01 +0300

    style: SFC swap FunctionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React, { SFC } from "react"
+import React, { FunctionComponent } from "react"
 import { Admin, Resource } from "react-admin"
 import jsonServerProvider from "ra-data-json-server"
 import { UserList } from "components/Users"
 import { createBrowserHistory } from "history"
 
 import authProvider from "providers/authProvider"
-
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com")
 const history = createBrowserHistory()
-const App: SFC<object> = () => (
+
+const App: FunctionComponent<object> = () => (
   <Admin
     authProvider={authProvider}
     dataProvider={dataProvider}
