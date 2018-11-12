94a3dc80b7bc3912aab7d66d2f5e8ac1b12dfd6a
Kirill Dvoynikov <illdv@mail.ru>
2018-11-12 12:16:35 +0300

    feat: add  CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import React, { SFC } from "react"
+import React, { FunctionComponent } from "react"
 import { Admin, Resource } from "react-admin"
 import jsonServerProvider from "ra-data-json-server"
 import { UserList } from "components/Users"
 import { createBrowserHistory } from "history"
+import CssBaseline from "@material-ui/core/CssBaseline"
 
 import authProvider from "providers/authProvider"
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com")
 const history = createBrowserHistory()
-const App: SFC<object> = () => (
-  <Admin
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    history={history}
-  >
-    <Resource name="users" list={UserList} />
-  </Admin>
+
+const App: FunctionComponent<object> = () => (
+  <>
+    <CssBaseline />
+    <Admin
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      history={history}
+    >
+      <Resource name="users" list={UserList} />
+    </Admin>
+  </>
 )
 
 export default App
